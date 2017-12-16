2abded26de3d385576e9766676b391418ecd218f
ameofoxil <33274466+ameofoxil@users.noreply.github.com>
2017-12-15 16:38:55 -0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import effay_bot
-
-def main():
-	bot = effay_bot.EffayBot("Mzc0MDc5NDY1MzI1NTkyNTc3.DNcFwA.1bshxhAf4a7QspQ2wErh6_XjTJ4", "373330633864249344", "373330634409771009")
-	print("starting . . .", end=" ")
-	bot.start()
-
-if(__name__ == "__main__"):
-	main()
